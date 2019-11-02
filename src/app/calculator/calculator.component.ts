24695b7a57d1cc0f45630712c329d8c34f450bf5
TeemuHe <teemuhoo@luukku.com>
2019-11-02 11:59:57 +0200

    calculator created

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,14 +6,29 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  buttons: string[];
+  prevValue: string;
+  private calculation: string;
 
   constructor() {
+    this.calculation = '';
+    this.prevValue = '';
+    this.buttons = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '+', 'C', '0', '=', '-'];
   }
-
-  buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '+', 'C', '0', '-', '='];
 
   ngOnInit() {
+
   }
 
+  addValue(value: string) {
+    if (value === '=') {
+      // tslint:disable-next-line:no-eval
+      this.calculation = eval(this.calculation);
+    } else if (value === 'C') {
+      this.calculation = '';
+    } else {
+      this.calculation += value;
+    }
+  }
 
 }
